6a50537f62f248fcad78de5926ccea1be7ec9dd7
Bjorn <capbarkley16@gmail.com>
2019-01-28 15:50:03 +0400

    adding project files

---
+++ script.js
@@ -0,0 +1,34 @@
+const h3 = document.getElementsByTagName("h3")[0];
+const body = document.getElementsByTagName("body")[0];
+const inputColor1 = document.querySelector("#inputColor1");
+const inputColor2 = document.getElementById("inputColor2");
+const btnToRight = document.querySelector("#btnToRight");
+const btnToLeft = document.querySelector("#btnToLeft");
+let directionOfGradient = "to right";
+function setGradientColor(){
+	body.style.background =
+	"linear-gradient(" + directionOfGradient + ", "
+	+ inputColor1.value
+	+ ", "
+	+ inputColor2.value
+	+ ")";
+
+	h3.textContent = body.style.background + ";";
+}
+
+// function setGradientDirection(){
+// 	directionOfGradient = 
+// }
+
+inputColor1.addEventListener("input", setGradientColor);
+inputColor2.addEventListener("input", setGradientColor);
+btnToRight.addEventListener("click", function(){
+	directionOfGradient = "to right";
+	setGradientColor();
+});
+btnToLeft.addEventListener("click", function(){
+	directionOfGradient = "to left";
+	setGradientColor();
+});
+
+console.log(body);
